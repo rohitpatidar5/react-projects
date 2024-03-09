c0bb1201ea9cc984dc9565d4238d69df33a02b24
Rohit Patidar <rohitpatidar583@gmail.com>
2024-03-09 23:57:31 +0530

    context api project

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Profile from './components/Profile'
+import Login from './components/Login'
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      <h1>React with Chai</h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
