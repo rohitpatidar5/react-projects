3663001ca76b13223dc2b29346bb932f44c932a0
Rohit Patidar <rohitpatidar583@gmail.com>
2024-03-10 21:44:17 +0530

    start learning react router

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,7 +7,7 @@ import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
